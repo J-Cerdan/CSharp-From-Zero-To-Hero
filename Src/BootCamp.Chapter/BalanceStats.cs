38e038788277e20fbbb16aed7f97c325c60a45b8
Joseph Cerdan <joseph.cerdan@hotmail.com>
2022-08-01 14:46:38 +1000

    Completed

--- Src/BootCamp.Chapter/BalanceStats.cs
+++ Src/BootCamp.Chapter/BalanceStats.cs
@@ -1,4 +1,9 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+using System.Text;
+using System.Globalization;
+
+namespace BootCamp.Chapter
 {
     public static class BalanceStats
     {
@@ -7,7 +12,57 @@
         /// </summary>
         public static string FindHighestBalanceEver(string[] peopleAndBalances)
         {
-            return "";
+            if (IsNullOrEmpty(peopleAndBalances)) return "N/A.";
+
+            string person="";
+            float highBalance = 0;
+            float value;
+            
+            int personCount = 1;
+            string[] people = new string[personCount];
+
+            for (int i=0; i<peopleAndBalances.Length; i++)
+            {
+                var personString = peopleAndBalances[i];
+                string[] personDetails = personString.Split(", ");
+                
+                for (int j=1; j<personDetails.Length; j++)
+                {
+                    string strVal = personDetails[j];
+
+                    value = float.Parse(strVal, CultureInfo.InvariantCulture.NumberFormat);
+
+                    if (value > highBalance)
+                    {
+                        personCount = 1;
+                        people = new string[personCount];
+
+                        highBalance = value;
+                        person = personDetails[0];
+
+                        people[0] = person;
+
+                    }
+                    else if (value == highBalance)
+                    {
+                        personCount++;
+                        string[] peopleTemp = new string[personCount];
+
+                        person = personDetails[0];
+
+                        people = Transfer(people, peopleTemp, person);
+                    }
+                }
+            }
+
+            var message = new StringBuilder();
+
+            message.Append(MessagePeopleBuild(people));
+
+            var formattedVal = String.Format(CultureInfo.InvariantCulture, "{0:C}", (int)highBalance);
+            message.Append($" had the most money ever. ¤{(int)highBalance}.");
+
+            return message.ToString();
         }
 
         /// <summary>
@@ -15,7 +70,81 @@
         /// </summary>
         public static string FindPersonWithBiggestLoss(string[] peopleAndBalances)
         {
-            return "";
+            if (IsNullOrEmpty(peopleAndBalances)) return "N/A.";
+
+            string person = "";
+            float currentBalance = 0;
+            float newVal;
+            float oldVal;
+            float diff = 0;
+            float maxLoss = 0;
+
+            int personCount = 1;
+            string[] people = new string[personCount];
+
+
+            for (int i = 0; i < peopleAndBalances.Length; i++)
+            {
+                var personString = peopleAndBalances[i];
+                string[] personDetails = personString.Split(", ");
+
+                if (personDetails.Length < 3) { continue; }
+
+                for (int j = 2; j < personDetails.Length; j++)
+                {
+                    string strVal = personDetails[j];
+                    string strVal2 = personDetails[j-1];
+                    newVal = float.Parse(strVal, CultureInfo.InvariantCulture.NumberFormat); 
+                    oldVal = float.Parse(strVal2, CultureInfo.InvariantCulture.NumberFormat);
+                    diff = newVal - oldVal;
+
+                    if (diff < maxLoss)
+                    {
+                        person = personDetails[0];
+                        maxLoss = diff;
+
+                        people[0] = person;
+                    }
+                    else if (diff == maxLoss)
+                    {
+                        personCount++;
+                        string[] peopleTemp = new string[personCount];
+
+                        person = personDetails[0];
+
+                        people = Transfer(people, peopleTemp, person);
+                    }
+                }
+
+            }
+
+            if (people[0]==null) return "N/A.";
+
+            var message = new StringBuilder();
+
+            message.Append(MessagePeopleBuild(people));
+
+            string str = "";
+
+            if ((int)maxLoss < 0)
+            {
+                str = $"-¤{Math.Abs(maxLoss)}";
+            }
+            else
+            {
+                str = $"¤{Math.Abs(maxLoss)}";
+            }
+
+            if (people.Length == 1)
+            {
+                message.Append($" lost the most money. {str}.");
+            }
+            else
+            {
+                message.Append($" have lost the most money. {str}.");
+            }
+
+            return message.ToString();
         }
 
         /// <summary>
@@ -23,7 +152,65 @@
         /// </summary>
         public static string FindRichestPerson(string[] peopleAndBalances)
         {
-            return "";
+            if (IsNullOrEmpty(peopleAndBalances)) return "N/A.";
+
+            string person = "";
+            float highBalance = 0;
+            float currentVal = 0;
+            float value;
+
+            int personCount = 1;
+            string[] people = new string[personCount];
+
+            for (int i = 0; i < peopleAndBalances.Length; i++)
+            {
+                var personString = peopleAndBalances[i];
+                string[] personDetails = personString.Split(", ");
+
+                for (int j = 1; j < personDetails.Length; j++)
+                {
+                    string strVal = personDetails[j];
+
+                    value = float.Parse(strVal, CultureInfo.InvariantCulture.NumberFormat);
+
+                    if (value > highBalance)
+                    {
+                        personCount = 1;
+                        people = new string[personCount];
+
+                        highBalance = value;
+                        person = personDetails[0];
+                        currentVal = float.Parse(personDetails[personDetails.Length-1], CultureInfo.InvariantCulture.NumberFormat);
+
+                        people[0] = person;
+
+                    }
+                    else if (value == highBalance)
+                    {
+                        personCount++;
+                        string[] peopleTemp = new string[personCount];
+
+                        person = personDetails[0];
+
+                        people = Transfer(people, peopleTemp, person);
+                    }
+                }
+            }
+
+            var message = new StringBuilder();
+
+            message.Append(MessagePeopleBuild(people));
+
+            if (people.Length == 1)
+            {
+                message.Append($" is the richest person. ¤{(int)currentVal}.");
+            }
+            else
+            {
+                message.Append($" are the richest people. ¤{(int)currentVal}.");
+            }
+
+            return message.ToString();
         }
 
         /// <summary>
@@ -31,7 +218,132 @@
         /// </summary>
         public static string FindMostPoorPerson(string[] peopleAndBalances)
         {
-            return "";
+
+            if (IsNullOrEmpty(peopleAndBalances)) return "N/A.";
+
+            string person = "";
+            float lowBalance = float.MaxValue;
+            float value;
+
+            int personCount = 1;
+            string[] people = new string[personCount];
+
+            for (int i = 0; i < peopleAndBalances.Length; i++)
+            {
+                var personString = peopleAndBalances[i];
+                string[] personDetails = personString.Split(", ");
+
+                value = float.Parse(personDetails[personDetails.Length - 1]);
+
+                if (value < lowBalance)
+                {
+                    personCount = 1; 
+                    people = new string[personCount];
+
+                    person = personDetails[0];
+                    lowBalance = float.Parse(personDetails[personDetails.Length - 1], CultureInfo.InvariantCulture.NumberFormat);
+
+                    people[0] = person;
+                }
+                else if (value == lowBalance)
+                {
+                    personCount++;
+                    string[] peopleTemp = new string[personCount];
+
+                    person = personDetails[0];
+
+                    people = Transfer(people, peopleTemp, person);
+                }
+
+
+            }
+
+            var message = new StringBuilder();
+
+            message.Append(MessagePeopleBuild(people));
+
+            string str = "";
+
+            if ((int)lowBalance < 0)
+            {
+                str = $"-¤{Math.Abs(lowBalance)}";
+            }
+            else
+            {
+                str = $"¤{Math.Abs(lowBalance)}";
+            }
+
+           
+
+            if (people.Length == 1)
+            {
+                message.Append($" has the least money. {str}.");
+            }
+            else
+            {
+                message.Append($" have the least money. {str}.");
+            }
+
+            return message.ToString();
+
+            return $"{person}, {lowBalance}";
+        }
+
+        private static string[] Transfer(string[] mainList, string[] tempList, string newAdd)
+        {
+            for (int i = 0; i < mainList.Length; i++)
+            {
+                tempList[i] = mainList[i];
+            }
+
+            tempList[tempList.Length - 1] = newAdd;
+
+            mainList = new string[tempList.Length];
+
+            for (int i = 0; i < tempList.Length; i++)
+            {
+                mainList[i] = tempList[i];
+            }
+
+            return mainList;
+        }
+
+        private static string MessagePeopleBuild(string[] peopleList)
+        {
+            var message = new StringBuilder();
+
+            if (peopleList.Length == 1)
+            {
+                message.Append(peopleList[0]);
+            }
+            else
+            {
+                for (int i = 0; i < peopleList.Length; i++)
+                {
+                    if (i == peopleList.Length - 2)
+                    {
+                        message.Append($"{peopleList[i]} and ");
+                    }
+                    else if (i == peopleList.Length - 1)
+                    {
+                        message.Append($"{peopleList[i]}");
+                    }
+                    else
+                    {
+                        message.Append($"{peopleList[i]}, ");
+                    }
+
+                }
+
+            }
+
+            return message.ToString();
+
+        }
+
+        private static bool IsNullOrEmpty(string[] array)
+        {
+            return (array == null || array.Length == 0);
         }
     }
 }
--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -1,4 +1,6 @@
-﻿namespace BootCamp.Chapter
+﻿using System.Text;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Part 1.
@@ -34,7 +36,99 @@
         /// </summary>
         public static string Build(string message, int padding)
         {
-            return "";
+            if (message == "") return "";
+
+            var lines = new StringBuilder();
+            var spaces = new StringBuilder();
+            int width = message.Length + (padding*2);
+            var final = new StringBuilder();
+            string[] messages = new string[1];
+            bool multiple = false;
+
+            int longest = 0;
+
+            if (message.Contains("\n"))
+            {
+                messages = message.Split("\n");
+                multiple = true;
+
+                foreach (string line in messages)
+                {
+                    if (longest < line.Length)
+                    {
+                        longest = line.Length;
+                    }
+                }
+
+            }
+
+            
+
+
+            //initialise spacing
+
+            if (multiple)
+            {
+                for (int i = 0; i < longest; i++)
+                {
+                    lines.Append("-");
+                    spaces.Append(" ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    lines.Append("-");
+                    spaces.Append(" ");
+                }
+            }
+            
+
+            //For top and bot
+            string edge = $"+{lines}+";
+
+            //padding bot
+            final.AppendLine(edge);
+
+
+            //for top and bot
+            string pad = $"|{spaces}|";
+
+            //padding top
+            for (int i = 0; i < padding; i++)
+            {
+                final.AppendLine(pad);
+            }
+
+            //message
+            if (multiple)
+            {
+                for (int i = 0; i < messages.Length; i++)
+                {
+                    message = messages[i].PadLeft(padding);
+                    message = message.PadRight(padding);
+                    final.AppendLine($"|{message}|");
+                }
+            }
+            else
+            {
+                message = message.PadLeft(padding + message.Length);
+                message = message.PadRight(padding + message.Length);
+                final.AppendLine($"|{message}|");
+            }
+            
+            
+            //bottom pad
+            for (int i = 0; i < padding; i++)
+            {
+                final.AppendLine(pad);
+            }
+
+            //bottom edge
+            final.AppendLine(edge);
+
+            return final.ToString();
         }
     }
 }
